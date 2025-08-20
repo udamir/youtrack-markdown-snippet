c816024f63716e65363577e1a772469d63a93590
Damir <damir@yusipov.ru>
2025-08-20 19:46:09 +0300

    move snippets list request to backend

--- src/backend-global.js
+++ src/backend-global.js
@@ -1,12 +1,66 @@
 const entities = require("@jetbrains/youtrack-scripting-api/entities");
+const http = require("@jetbrains/youtrack-scripting-api/http");
 
-const isArticle = (entityId) => {
-  const [, i, a] = entityId.split("-");
-  return i === "A" && a;
-};
+/** 
+ * Checks if the entity is an article
+ * @param {string} entityId - The ID of the entity
+ * @returns {boolean} - True if the entity is an article, false otherwise
+*/
+const isArticle = (entityId) => entityId.split("-")[1] === "A";
 
+/**
+ * @import { YouTrackGlobalContext } from "../@types/context";
+ * 
+ * @type {{
+ *  endpoints: {
+ *    method: "GET" | "POST" | "PUT" | "DELETE";
+ *    path: string;
+ *    handle: (ctx: YouTrackGlobalContext) => void;
+ *  }[]
+ * }} 
+*/
 exports.httpHandler = {
   endpoints: [
+    {
+      method: "GET",
+      path: "snippets",
+      handle: (ctx) => {
+        const baseUrl = ctx.globalStorage.extensionProperties.baseUrl;
+        const apiKey = ctx.globalStorage.extensionProperties.apiKey;
+
+        if (!baseUrl || !apiKey) {
+          ctx.response.code = 500;
+          ctx.response.json({
+            message: "Missing baseUrl or apiKey",
+          });
+          return;
+        }
+
+        const connection = new http.Connection(baseUrl.endsWith('/') ? baseUrl : `${baseUrl}/`, null, 30000);
+        connection.bearerAuth(apiKey);
+
+        const response = connection.getSync("/api/admin/workflows", { fields: "id,name,rules(id,name,title,type)", $top: "-1", query: "language:JS,mps" });
+        const json = JSON.parse(response.response);
+        if (!response.isSuccess) {
+          ctx.response.code = 500;
+          ctx.response.json({
+            message: `Cannot fetch list of snippets: ${json.message}`,
+          });
+          return;
+        }
+
+        const snippets = [];
+        for (const workflow of json) {
+          for (const rule of workflow.rules) {
+            if (rule.title?.startsWith("snippet:") && rule.type === "StatelessActionRule") {
+              snippets.push({ title: rule.title.slice(8), workflow: workflow.name, rule: rule.name })
+            }
+          }
+        }
+
+        ctx.response.json({ snippets });
+      }
+    },
     {
       method: "GET",
       path: "snippet",
@@ -29,7 +83,7 @@ exports.httpHandler = {
             article, 
             issue, 
             currentUser, 
-            setUserInput: (value) => {
+            setUserInput: (/** @type {any} */ value) => {
               input = value;
             } 
           });
@@ -44,7 +98,7 @@ exports.httpHandler = {
               ...(input ? { input } : {})
             });
           }
-        } catch (error) {
+        } catch (/** @type {any} */ error) {
           ctx.response.code = 500;
           ctx.response.json({
             message: `Error in workflow rule: "${workflow}/${ruleName}.js"`,
